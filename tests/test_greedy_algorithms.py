18f1685e810c044d6096ea6bec8569957d841047
xavi <xpeiro92@gmail.com>
2022-06-29 07:47:53 +0200

    tests max loot

--- tests/test_greedy_algorithms.py
+++ tests/test_greedy_algorithms.py
@@ -1,4 +1,4 @@
-from tasks.greedy_algorithims import money_change
+from tasks.greedy_algorithims import money_change, max_value_loot
 
 
 def test_money_change():
@@ -9,4 +9,17 @@ def test_money_change():
     assert money_change(3) == 3
     assert money_change(8) == 4
 
+
+def test_max_value_of_loot():
+    items_store_1 = {
+        60: 20,
+        100: 50,
+        120: 30
+    }
+    items_store_2 = {
+        500: 30,
+    }
+    assert max_value_loot(prods_count=3, bag_items_capacity=50, prods=items_store_1) == 180.0
+    assert max_value_loot(prods_count=3, bag_items_capacity=30, prods=items_store_1) == 120.0
+    assert max_value_loot(prods_count=1, bag_items_capacity=10, prods=items_store_2) == 166.6667
 
